9d55e2e02fcefc4051c516bc79a51b1881a58f48
konstantinlevin <tekmanmehmetyasar@gmail.com>
2021-06-18 20:42:08 +0300

    id parameter has been added to the constructor of TodoList, ListEntry class entity has been written.

---
+++ entities/ListEntry.py
@@ -0,0 +1,7 @@
+class ListEntry():
+
+    def __init__(self,list_id,entry_text,done,id=None):
+        self.list_id = list_id
+        self.entry_text = entry_text
+        self.done = done
+        self.id = id
--- entities/TodoList.py
+++ entities/TodoList.py
@@ -1,7 +1,7 @@
 class TodoList():
 
-    def __init__(self,name,password):
-        self.id = None
+    def __init__(self,name,password,id=None):
+        self.id = id
         self.name = name
         self.password = password
 
