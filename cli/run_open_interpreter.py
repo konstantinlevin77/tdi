e52705390d12c0e92e1a4c154e9750df24f52de2
konstantinlevin <tekmanmehmetyasar@gmail.com>
2021-07-03 20:50:19 +0300

    Basics of open command interpreter has been written.

--- cli/run_open_interpreter.py
+++ cli/run_open_interpreter.py
@@ -1,17 +1,40 @@
-def done_command():
+from colorama import Fore
+
+def done_command(args):
     pass
 
-def undone_command():
+def undone_command(args):
     pass
 
-def add_command():
+def add_command(args):
     pass
 
-def remove_command():
+def remove_command(args):
     pass
 
-def exit_command():
+def exit_command(args):
     pass
+
+interpreter_commands = {
+    "done":done_command,
+    "undone":undone_command,
+    "add":add_command,
+    "remove":remove_command,
+    "exit":exit_command
+}
 
 def run_open_interpreter():
-    command = input()
+    while True:
+
+        raw_command = input(Fore.YELLOW+"Type your command: "+Fore.RESET)
+        command,args = raw_command.split()[0],raw_command.split()[1:len(raw_command.split())]
+
+        command_as_function = interpreter_commands.get(command)
+
+        if command_as_function is None:
+            print(Fore.CYAN+f"There is no command called {command}")
+
+        else:
+            command_as_function(args)
+        
+        
